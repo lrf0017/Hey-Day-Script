d4a69d6b30d3aa3a24579ddaf797617b7554783b
lrf0017 <145815558+lrf0017@users.noreply.github.com>
2023-10-17 19:59:12 -0500

    Add files via upload

---
+++ heyday.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter your name."
+
+#read the name typed in by the user
+read name 
+
+#then print hey with whatever name was entered
+echo "Hey, $name! Happy Hey Day 2023!" 
+
+
+
